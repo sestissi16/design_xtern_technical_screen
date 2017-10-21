e82b9b23a931196f3cffb1fb8279239168c21039
Sari Stissi <sestissi16@earlham.edu>
2017-10-21 18:27:34 -0400

    Made Header stay fixed at top and had sidebar appear underneath it

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+    return(
+        <header className="App-header">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <h1 className="App-title">OSXtern</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './SideBar.css'
+
+const SideBar = ()=>{
+    return (
+       <nav className="Sidebar">
+           <div className="Title">I'm a sidebar</div>
+       </nav> 
+    )
+}
+
+export default SideBar
