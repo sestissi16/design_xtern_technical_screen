44c40284c36dc08f351c1b1a39675c79a73d69da
Sari Stissi <sestissi16@earlham.edu>
2017-10-23 07:56:23 -0400

    Added xtern logo to header

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ import './Header.css'
 const Header = () => {
     return(
         <header className="App-header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQckFsrLaBf_-Q97dk9Swl_LNQZ4lkwKMBR82wadIs0dg9dT_VS" alt="Xtern logo" className="imagelogo"/>
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <h1 className="App-title">OSXtern</h1>
         </header>
