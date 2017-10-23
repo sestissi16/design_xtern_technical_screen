f6ea1a419c04b74a7cb0c7fefce811e1e28a54c9
Sari Stissi <sestissi16@earlham.edu>
2017-10-23 09:18:56 -0400

    Added styling to the title

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,11 @@ const Header = () => {
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQckFsrLaBf_-Q97dk9Swl_LNQZ4lkwKMBR82wadIs0dg9dT_VS" alt="Xtern logo" className="imagelogo"/>
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <h1 className="App-title">OSXtern</h1>
+            <button
+                type="button"
+                className="SignoutButton">
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+            </button>
         </header>
     )
 }
